55a5dda41ae44e96a109cd35b094a5aa630fa567
Carlos Guisao <cguisao@fau.edu>
2019-02-14 00:43:37 -0500

    Initial check in, all of the functionality works as far as login and users roles

---
+++ GTI_Solutionx/GTI_Solutionx/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GTI_Solutionx.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace GTI_Solutionx.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Authorize(Roles = "Admin, User")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
