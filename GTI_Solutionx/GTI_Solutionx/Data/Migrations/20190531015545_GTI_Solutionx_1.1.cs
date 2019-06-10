a72cad551eb669a9436cdb246e2bc899c47086cd
Carlos Guisao <cguisao@fau.edu>
2019-06-10 17:55:59 -0400

    New files future enhancements

---
+++ GTI_Solutionx/GTI_Solutionx/Data/Migrations/20190531015545_GTI_Solutionx_1.1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GTI_Solutionx.Data.Migrations
+{
+    public partial class GTI_Solutionx_11 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "marketPlace",
+                table: "Amazon",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "marketPlace",
+                table: "Amazon");
+        }
+    }
+}
